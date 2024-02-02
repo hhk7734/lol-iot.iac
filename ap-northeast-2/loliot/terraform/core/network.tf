2c29e43df65f7ff652f06e2bf7d2c74ce9e86d01
Hyeonki Hong <hhk7734@gmail.com>
2024-02-03 02:52:32 +0900

    feat(loliot/tf/core): import cilium

---
+++ ap-northeast-2/loliot/terraform/core/network.tf
@@ -0,0 +1,47 @@
+resource "helm_release" "cilium" {
+  chart       = "../../../../charts/cilium-1.14.6.tgz"
+  max_history = 3
+  name        = "cilium"
+  namespace   = "kube-system"
+  repository  = null
+  timeout     = 300
+  values = [jsonencode(
+    {
+      k8sServiceHost = "10.255.240.2"
+      k8sServicePort = "6443"
+      cluster = {
+        name = "ap-northeast-2-loliot-net"
+      }
+      ipam = {
+        mode = "cluster-pool"
+        operator = {
+          clusterPoolIPv4MaskSize = 25
+          clusterPoolIPv4PodCIDRList = [
+            "10.244.0.0/16",
+          ]
+        }
+      },
+      kubeProxyReplacement = "true"
+      operator = {
+        replicas = 1
+      }
+    }
+  )]
+  wait = true
+}
+
+resource "kubernetes_manifest" "cilium-loadbalancer-ip-pool" {
+  manifest = {
+    apiVersion = "cilium.io/v2alpha1"
+    kind       = "CiliumLoadBalancerIPPool"
+    metadata = {
+      name = "cilium-loadbalancer-ip-pool"
+    }
+    spec = {
+      cidrs = [{
+        cidr = "172.26.0.0/20"
+      }]
+    }
+  }
+  depends_on = [helm_release.cilium]
+}
