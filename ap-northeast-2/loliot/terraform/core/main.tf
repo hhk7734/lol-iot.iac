5e585139e70f0a5e841650229fab024436b2d0ea
Hyeonki Hong <hhk7734@gmail.com>
2024-02-04 00:46:40 +0900

    chore(loliot/tf/core): add local.cluster_dir

--- ap-northeast-2/loliot/terraform/core/main.tf
+++ ap-northeast-2/loliot/terraform/core/main.tf
@@ -14,22 +14,23 @@ terraform {
   required_version = ">= 1.7, < 2.0"
 
   backend "local" {
-    path = "../../../../local_secret/ap-northeast-2/loliot/terraform/core/terraform.tfstate"
+    path = "${local.cluster_dir}/terraform/core/terraform.tfstate"
   }
 }
 
 provider "kubernetes" {
-  config_path = "../../../../local_secret/ap-northeast-2/loliot/k0s/kubeconfig"
+  config_path = "${local.cluster_dir}/k0s/kubeconfig"
 }
 
 provider "helm" {
   kubernetes {
-    config_path = "../../../../local_secret/ap-northeast-2/loliot/k0s/kubeconfig"
+    config_path = "${local.cluster_dir}/k0s/kubeconfig"
   }
 }
 
 locals {
-  charts_dir = "${path.root}/../../../../charts"
+  cluster_dir = "${path.root}/../../../../local_secret/ap-northeast-2/loliot"
+  charts_dir  = "${path.root}/../../../../charts"
 
   control_plane_node_affinity = {
     requiredDuringSchedulingIgnoredDuringExecution = {
