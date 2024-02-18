65f1731dfae651390ff89167c63e759ef53f2053
Hyeonki Hong <hhk7734@gmail.com>
2024-02-19 01:07:09 +0900

    feat(loliot/tf/core): add rook-ceph-cluster

--- ap-northeast-2/loliot/terraform/core/storage.tf
+++ ap-northeast-2/loliot/terraform/core/storage.tf
@@ -66,7 +66,142 @@ resource "helm_release" "rook-ceph" {
   namespace   = kubernetes_namespace.rook-ceph.metadata[0].name
   timeout     = 300
   values = [
-    jsonencode({})
+    jsonencode({
+      csi = {
+        enableRbdDriver              = false
+        csiCephFSProvisionerResource = <<-EOT
+            - name : csi-provisioner
+              resource: null
+            - name : csi-resizer
+              resource: null
+            - name : csi-attacher
+              resource: null
+            - name : csi-snapshotter
+              resource: null
+            - name : csi-cephfsplugin
+              resource: null
+            - name : liveness-prometheus
+              resource: null
+          EOT
+        csiCephFSPluginResource      = <<-EOT
+            - name : driver-registrar
+              resource: null
+            - name : csi-cephfsplugin
+              resource: null
+            - name : liveness-prometheus
+              resource: null
+          EOT
+        kubeletDirPath               = "/var/lib/k0s/kubelet"
+      }
+    })
   ]
   wait = true
+}
+
+resource "helm_release" "rook-ceph-cluster" {
+  chart       = "${local.charts_dir}/rook-ceph-cluster-v1.13.4.tgz"
+  max_history = 3
+  name        = "rook-ceph-cluster"
+  namespace   = kubernetes_namespace.rook-ceph.metadata[0].name
+  timeout     = 300
+  values = [jsonencode(
+    {
+      operatorNamespace = kubernetes_namespace.rook-ceph.metadata[0].name
+      cephClusterSpec = {
+        mon = {
+          count = 1
+        }
+        mgr = {
+          count = 1
+        }
+        dashboard = {
+          enabled = true
+          ssl     = false
+        }
+        logCollector = {
+          enabled = false
+        }
+        resources = {
+          mgr = {
+            requests = null
+            limits   = null
+          },
+          mon = {
+            requests = null
+            limits   = null
+          },
+          osd = {
+            requests = null
+            limits   = null
+          }
+        }
+        storage = {
+          useAllNodes   = false
+          useAllDevices = false
+          nodes = [
+            {
+              name = "ip-10-255-240-4"
+              devices = [
+                {
+                  name = "/dev/vg1/lv1"
+                }
+              ]
+            }
+          ]
+        }
+      }
+      cephBlockPools = []
+      cephFileSystems = [
+        {
+          name = "ceph-filesystem"
+          spec = {
+            metadataPool = {
+              replicated = {
+                size = 1
+              }
+            }
+            dataPools = [
+              {
+                failureDomain = "host"
+                replicated = {
+                  size = 1
+                }
+                name = "data0"
+              }
+            ]
+            metadataServer = {
+              activeCount   = 1
+              activeStandby = true
+              resources = {
+                requests = null
+                limits   = null
+              }
+            }
+          }
+          storageClass = {
+            enabled              = true
+            isDefault            = false
+            name                 = "ceph-filesystem"
+            pool                 = "data0"
+            reclaimPolicy        = "Delete"
+            allowVolumeExpansion = true
+            volumeBindingMode    = "Immediate"
+            mountOptions         = []
+            parameters = {
+              "csi.storage.k8s.io/provisioner-secret-name"            = "rook-csi-cephfs-provisioner"
+              "csi.storage.k8s.io/provisioner-secret-namespace"       = "{{ .Release.Namespace }}"
+              "csi.storage.k8s.io/controller-expand-secret-name"      = "rook-csi-cephfs-provisioner"
+              "csi.storage.k8s.io/controller-expand-secret-namespace" = "{{ .Release.Namespace }}"
+              "csi.storage.k8s.io/node-stage-secret-name"             = "rook-csi-cephfs-node"
+              "csi.storage.k8s.io/node-stage-secret-namespace"        = "{{ .Release.Namespace }}"
+              "csi.storage.k8s.io/fstype"                             = "ext4"
+            }
+          }
+        }
+      ]
+      cephObjectStores = []
+    }
+  )]
+  wait       = true
+  depends_on = [helm_release.rook-ceph]
 }
