6b65fa0a16b672708a139088967ab845a05a3b30
Hyeonki Hong <hhk7734@gmail.com>
2024-03-09 01:53:27 +0900

    feat(home/lol-iot/terragrunt): add CiliumL2AnnouncementPolicy

--- home/lol-iot/terragrunt/core/network/cilium-cr/cr.tf
+++ home/lol-iot/terragrunt/core/network/cilium-cr/cr.tf
@@ -1,3 +1,17 @@
+resource "kubernetes_manifest" "cilium-l2-announcement-policy" {
+  manifest = {
+    apiVersion = "cilium.io/v2alpha1"
+    kind       = "CiliumL2AnnouncementPolicy"
+    metadata = {
+      name = "cilium-l2-announcement-policy"
+    }
+    spec = {
+      externalIPs     = true
+      loadBalancerIPs = true
+    }
+  }
+}
+
 resource "kubernetes_manifest" "cilium-loadbalancer-ip-pool" {
   manifest = {
     apiVersion = "cilium.io/v2alpha1"
@@ -6,9 +20,14 @@ resource "kubernetes_manifest" "cilium-loadbalancer-ip-pool" {
       name = "cilium-loadbalancer-ip-pool"
     }
     spec = {
-      cidrs = [{
-        cidr = "192.168.0.0/24"
-      }]
+      cidrs = [
+        {
+          cidr = "192.168.0.208/28" # 192.168.0.208 ~ 192.168.0.223
+        },
+        {
+          cidr = "192.168.0.224/28" # 192.168.0.224 ~ 192.168.0.239
+        }
+      ]
     }
   }
 }
