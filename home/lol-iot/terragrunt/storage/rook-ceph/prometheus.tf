cb93ac6dc6e6bf3fa8b8729c59b9f941246a3d67
Hyeonki Hong <hhk7734@gmail.com>
2024-03-16 00:35:30 +0900

    feat(home/lol-iot/terragrunt): add kubernetes_cluster_role.rook-ceph-prometheus

--- home/lol-iot/terragrunt/storage/rook-ceph/prometheus.tf
+++ home/lol-iot/terragrunt/storage/rook-ceph/prometheus.tf
@@ -5,14 +5,39 @@ resource "kubernetes_service_account" "prometheus" {
   }
 }
 
-resource "kubernetes_cluster_role_binding" "prometheus" {
+resource "kubernetes_cluster_role" "rook-ceph-prometheus" {
+  metadata {
+    name = "rook-ceph-prometheus"
+  }
+  rule {
+    api_groups = [""]
+    resources  = ["nodes", "nodes/metrics", "services", "endpoints", "pods"]
+    verbs      = ["get", "list", "watch"]
+  }
+  rule {
+    api_groups = [""]
+    resources  = ["configmaps"]
+    verbs      = ["get"]
+  }
+  rule {
+    api_groups = ["networking.k8s.io"]
+    resources  = ["ingresses"]
+    verbs      = ["get", "list", "watch"]
+  }
+  rule {
+    non_resource_urls = ["/metrics"]
+    verbs             = ["get"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "rook-ceph-prometheus" {
   metadata {
     name = "rook-ceph-prometheus"
   }
   role_ref {
     api_group = "rbac.authorization.k8s.io"
     kind      = "ClusterRole"
-    name      = "prometheus"
+    name      = kubernetes_cluster_role.rook-ceph-prometheus.metadata[0].name
   }
   subject {
     kind      = "ServiceAccount"
