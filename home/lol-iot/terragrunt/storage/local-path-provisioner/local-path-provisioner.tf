d7cc99754558374ab24f07040f956707d98db009
Hyeonki Hong <hhk7734@gmail.com>
2024-11-29 21:04:53 +0900

    feat(home/lol-iot/terragrunt): apply storage/local-path-provisioner

--- home/lol-iot/terragrunt/storage/local-path-provisioner/local-path-provisioner.tf
+++ home/lol-iot/terragrunt/storage/local-path-provisioner/local-path-provisioner.tf
@@ -1,17 +1,23 @@
-resource "kubernetes_namespace" "local-path-provisioner" {
-  metadata {
-    name = "local-path-provisioner"
-  }
-}
-
-resource "helm_release" "local-path-provisioner" {
+resource "helm_release" "local_path_provisioner" {
   repository  = "https://hhk7734.github.io/helm-charts/"
   chart       = "local-path-provisioner"
-  version     = "0.0.26"
+  version     = "0.0.30"
   max_history = 5
   name        = "local-path-provisioner"
-  namespace   = kubernetes_namespace.local-path-provisioner.metadata[0].name
+  namespace   = "kube-system"
   timeout     = 300
-  values      = []
-  wait        = true
+  values = [jsonencode({
+    tolerations = [
+      {
+        key      = "node-role.kubernetes.io/control-plane"
+        operator = "Exists"
+      },
+      {
+        key      = "loliot.net/storage"
+        operator = "Equal"
+        value    = "enabled"
+        effect   = "NoSchedule"
+      }
+    ]
+  })]
 }
--- home/lol-iot/terragrunt/storage/rook-ceph/terragrunt.hcl
+++ home/lol-iot/terragrunt/storage/rook-ceph/terragrunt.hcl
@@ -5,3 +5,14 @@ include "base" {
 include "kubernetes" {
   path = find_in_parent_folders("kubernetes.hcl")
 }
+
+locals {
+  k8s_dir = get_parent_terragrunt_dir("kubernetes")
+}
+
+dependencies {
+  paths = [
+    "${local.k8s_dir}/network/cilium",
+    "${local.k8s_dir}/storage/local-path-provisioner"
+  ]
+}
