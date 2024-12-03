b169a87623390457a6860dbd1d2d24b3f444518a
Hyeonki Hong <hhk7734@gmail.com>
2024-12-03 22:22:27 +0900

    feat(home/lol-iot/terragrunt): remove tolerations from casdoor

--- home/lol-iot/terragrunt/auth/casdoor/casdoor.tf
+++ home/lol-iot/terragrunt/auth/casdoor/casdoor.tf
@@ -20,17 +20,5 @@ resource "helm_release" "casdoor" {
       password = rsadecrypt("eI/1FvpJrqi2IIkNY7f4RBTj23usF0473dSEkTLV+tYlbjn/aNJ/FLXjaGa4nupUvREaJl8miNUW68w0wVCfEy364oA63+L0l1NqTW/m2w018mbcNq7idlmg5Vvj7J94HCBqqsd591anhKT41GKF7vtCdwIudOASiod57EIv0Smhi8/yaHPv/jKUNtSct+5ncJAp1zrUPAh1FIDhL3JRsLFbzP5jd70fTSwAUbLksHX/twOnw6wS5yhDrJ54lLYxnNINSNq5B03R/X12Uz1HOIlgENoxuMTBS9cLvBowmiNhjqJn63SDelQew+ma4wSHi3v/rxBkrpUEysJIcms6Mz1LY7XPQvpSVccZTBQTcOssO2XW9FWWWvibx4z3hVfx94cDGJ2dkTGqrh8B9IRcjbENgcKt3/mOpDdVqHtsJuz8oP8DDwXa5u1WyXRe2I+ZOASAEbnARCbUYQ2bwNj62Smgll0+Ebwuv1D8wWGZItZKI3lvfXLgZzKBac4KQh3eC/I1JsN1Q8z20/rIW7XCJZxdXv2AkOxpYuN61urWwPrpeNc/OQXVeYXzNN1I4EdKPp3THrtvFtvovLGD4ZxoK8t11iM46SYd71Snis1bDeR8RTAjHYcjstg+BI3l4vUYmW+SXA/zS1CAXgg/BpEJOmZS48RJPRn+8bNjTemv1VA=", local.private_pem)
       host     = "postgres.postgres.svc.cluster.local"
     }
-    tolerations = [
-      {
-        key      = "node-role.kubernetes.io/control-plane"
-        operator = "Exists"
-      },
-      {
-        key      = "loliot.net/storage"
-        operator = "Equal"
-        value    = "enabled"
-        effect   = "NoSchedule"
-      }
-    ]
   })]
 }
