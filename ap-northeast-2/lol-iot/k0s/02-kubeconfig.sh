8cc5ef658d3a9070a8f0c6901490d7313955768f
Hyeonki Hong <hhk7734@gmail.com>
2024-12-05 22:59:06 +0900

    chore: remove ap-northeast-2

--- ap-northeast-2/lol-iot/k0s/02-kubeconfig.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -e
-set -x
-
-sudo k0s kubeconfig create admin --groups system:masters >kubeconfig
--- ap-northeast-2/lol-iot/k0s/03-gen-worker-token.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-set -x
-
-sudo k0s token create --role=worker --expiry=24h >token
-
-cat token | base64 -d | gunzip -
